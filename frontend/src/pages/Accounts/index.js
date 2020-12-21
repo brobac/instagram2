fda4d4520144d46f068c0d3ad4ec57b6009ba601
park <issg2030@gmail.com>
2020-12-21 15:40:47 +0900

    s

---
+++ frontend/src/pages/Accounts/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AccountRoute() {
+  return <div>그마아아아안</div>;
+}
+
+export default AccountRoute;
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import AccountRoute from "./Accounts";
 import Home from "./Home";
 function Root() {
   return (
     <div>
       <Route exact path="/" component={Home} />
+      <Route exact path="/accounts/" component={AccountRoute} />
     </div>
   );
 }
